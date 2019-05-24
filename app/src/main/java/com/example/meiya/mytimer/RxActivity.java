f0e4002716ebf346586d3be961c88d74ac99d76f
jamesm <leisendi@icloud.com>
2019-05-24 13:56:25 +0800

    Create by James.c

--- app/src/main/java/com/example/meiya/mytimer/RxActivity.java
+++ app/src/main/java/com/example/meiya/mytimer/RxActivity.java
@@ -1,6 +1,5 @@
 package com.example.meiya.mytimer;
 
-import android.app.ProgressDialog;
 import android.content.Context;
 import android.os.Bundle;
 import android.os.Messenger;
@@ -8,7 +7,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
